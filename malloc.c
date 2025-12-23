90cd34181fdf5131cc225eaa945e9524cf3d3155
Daniel Hirt <danielchirt16@gmail.com>
2025-12-23 13:02:11 -0500

    implement malloc in c

---
+++ malloc.c
@@ -0,0 +1,124 @@
+#include <assert.h>
+#include <endian.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct block_meta {
+  size_t size;
+  struct block_meta *next;
+  int free;
+  int magic; // for debugging
+};
+
+#define META_SIZE sizeof(struct block_meta)
+
+void *global_base = NULL;
+
+struct block_meta *find_free_block(struct block_meta **last, size_t size) {
+  struct block_meta *current = global_base;
+  while (current && !(current->free && current->size >= size)) {
+    *last = current;
+    current = current->next;
+  }
+  return current;
+}
+
+struct block_meta *request_space(struct block_meta *last, size_t size) {
+  struct block_meta *block;
+  block = sbrk(0);
+  void *request = sbrk(size + META_SIZE);
+  assert((void *)block == request); // not thread safe
+  if (request == (void *)-1) {
+    return NULL;
+  }
+
+  if (last) {
+    last->next = block;
+  }
+  block->size = size;
+  block->next = NULL;
+  block->free = 0;
+  block->magic = 0x12345678;
+  return block;
+}
+
+struct block_meta *get_block_ptr(void *ptr) {
+  return (struct block_meta *)ptr - 1;
+}
+
+void *malloc(size_t size) {
+  struct block_meta *block;
+  // TODO: align size?
+
+  if (size <= 0) {
+    return NULL;
+  }
+
+  if (!global_base) {
+    block = request_space(NULL, size);
+    if (!block) {
+      return NULL;
+    }
+    global_base = block;
+  } else {
+    struct block_meta *last = global_base;
+    block = find_free_block(&last, size);
+    if (!block) { // failed to find a free block
+      block = request_space(last, size);
+      if (!block) {
+        return NULL;
+      }
+    } else {
+      block->free = 0;
+      block->magic = 0x77777777;
+    }
+  }
+  return (block + 1); // we return block+1 because we want to return a pointer
+                      // to the region after block_meta. Since block is a
+                      // pointer of type struct block_meta, +1 increments the
+                      // address by one sizeof(struct block_meta)
+}
+
+void free(void *ptr) {
+  if (!ptr) {
+    return;
+  }
+
+  struct block_meta *block_ptr = get_block_ptr(ptr);
+  assert(block_ptr->free == 0);
+  assert(block_ptr->magic == 0x77777777 || block_ptr->magic == 0x12345678);
+  block_ptr->free = 1;
+  block_ptr->magic = 0x55555555;
+}
+
+void *calloc(size_t nelem, size_t elsize) {
+  size_t size = nelem * elsize; // TODO: check for overflow.
+  void *ptr = malloc(size);
+  memset(ptr, 0, size);
+  return ptr;
+}
+
+void *realloc(void *ptr, size_t size) {
+  if (!ptr) {
+    // NULL ptr. realloc should act like malloc.
+    return malloc(size);
+  }
+
+  struct block_meta *block_ptr = get_block_ptr(ptr);
+  if (block_ptr->size >= size) {
+    // We have enough space. Could free some once we implement split.
+    return ptr;
+  }
+
+  // Need to really realloc. Malloc new space and free old space.
+  // Then copy old data to new space.
+  void *new_ptr;
+  new_ptr = malloc(size);
+  if (!new_ptr) {
+    return NULL; // TODO: set errno on failure.
+  }
+  memcpy(new_ptr, ptr, block_ptr->size);
+  free(ptr);
+  return new_ptr;
+}
